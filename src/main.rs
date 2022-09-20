46c07ac2d56309fdca4587400eb39d6d710c1737
Jason Carpenter <5916026+breadbored@users.noreply.github.com>
2022-09-19 22:24:25 -0400

    Refactor listeners to Server impl

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,17 @@
 mod packet;
 mod server;
 mod client;
-use tokio::{
-    net::{
-        TcpSocket,
-        TcpStream,
-        TcpListener
-    },
-    io::{
-        AsyncReadExt,
-        AsyncWriteExt
-    }
-};
-use std::{
-    io::{prelude::*, BufReader, Result}, borrow::Borrow,
-};
+use server::{Server, ServerTraits};
+use tokio::net::TcpListener;
+use std::io::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let addr = "0.0.0.0:1027";
-
     let listener: TcpListener = TcpListener::bind(addr).await.unwrap();
+
+    let server = Server::new(listener);
     
-    loop {
-        let (mut socket, socket_addr) = listener.accept().await?;
-        println!("new client: {:?}", socket_addr.to_string());
-
-        tokio::spawn(async move {
-            handle_socket(socket).await;
-        });
-    }
-}
-
-async fn handle_socket(mut socket: TcpStream) {
-    let mut buffer: [u8; 128] = [0; 128];
-    // In a loop, read data from the socket and write the data back.
-    loop {
-        let n = socket
-            .read(&mut buffer)
-            .await
-            .expect("failed to read data from socket");
-
-        if n == 0 {
-            continue;
-        }
-
-        let mut temp_buffer = &buffer[0..n];
-
-        print!("{:?}", temp_buffer);
-
-        socket
-            .write_all(temp_buffer)
-            .await
-            .expect("failed to write data to socket");
-    }
+    server.start().await?;
+    Ok(())
 }
--- src/server.rs
+++ src/server.rs
@@ -1,21 +1,76 @@
-use std::{net::TcpStream, cell::RefCell, rc::Weak};
-use uuid::Uuid;
-use chrono::{
-    DateTime,
-    Utc
+use tokio::{
+    net::{
+        TcpStream,
+        TcpListener
+    },
+    io::{
+        AsyncReadExt,
+        AsyncWriteExt,
+        Result
+    }
 };
-use crate::{packet::packets::{
-    GamePacket::GamePacket,
-    CostumePacket::CostumePacket
-}, client::Client};
+use mempool::Pool;
+use crate::client::Client;
 
 pub struct Server<'a> {
-    pub clients: Vec<Client<'a>>
+    pub listener: TcpListener,
+    pub clients: Vec<Client<'a>>,
+    pub mempool: Pool<[u8; 1024]>,
 }
-pub trait ServerTraits {
 
+pub trait ServerTraits {
+    fn new(listener: TcpListener) -> Self;
 }
 
 impl ServerTraits for Server<'_> {
+    fn new(listener: TcpListener) -> Self {
+        Server {
+            listener: listener,
+            clients: vec![],
+            mempool: Pool::new(Box::new(|| [0; 1024])),
+        }
+    }
+}
+impl Server<'_> {
+    pub async fn start(&self) -> Result<()> {
+        // Loop until new connection is made and spawn an async event loop
+        loop {
+            let (mut socket, socket_addr) = self.listener.accept().await?;
+            println!("new client: {:?}", socket_addr.to_string());
 
+            tokio::spawn(async move {
+                Server::handle_socket(socket).await;
+            });
+
+            // Trick the compiler into thinking this eventually responds with Okay(())
+            if false {
+                break
+            };
+        }
+        return Ok(())
+    }
+
+    async fn handle_socket(mut socket: TcpStream) {
+        let mut buffer: [u8; 128] = [0; 128];
+        // In a loop, read data from the socket and write the data back.
+        loop {
+            let n = socket
+                .read(&mut buffer)
+                .await
+                .expect("failed to read data from socket");
+    
+            if n == 0 {
+                continue;
+            }
+    
+            let mut temp_buffer = &buffer[0..n];
+    
+            print!("{:?}", temp_buffer);
+    
+            socket
+                .write_all(temp_buffer)
+                .await
+                .expect("failed to write data to socket");
+        }
+    }
 }
